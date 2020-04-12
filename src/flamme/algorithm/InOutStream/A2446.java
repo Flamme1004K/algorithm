31d04719fe677f855d1c089930b982bab295ca3a
flamme <flamme1004@gmail.com>
2020-04-12 17:31:14 +0900

    오늘자 알고리즘

--- src/flamme/algorithm/InOutStream/A2446.java
+++ src/flamme/algorithm/InOutStream/A2446.java
@@ -1,13 +1,24 @@
 package flamme.algorithm.InOutStream;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 
 public class A2446 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int input = Integer.parseInt(br.readLine());
+
+        for(int i = 1; i < input; i++) {
+            for(int j = 1; j < i; j++) bw.write(" ");
+            for(int j = 0; j < (input - i)*2 +1; j++) bw.write("*");
+            bw.write("\n");
+        }
+        for(int i = 0; i < input; i++) {
+            for(int j = 1; j < input - i; j++) bw.write(" ");
+            for(int j = 0; j < i * 2 + 1; j++) bw.write("*");
+            bw.write("\n");
+        }
+
+        bw.flush();
     }
 }
--- src/flamme/algorithm/Main.java
+++ src/flamme/algorithm/Main.java
@@ -1,27 +1,24 @@
 package flamme.algorithm;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int input = Integer.parseInt(br.readLine());
-        StringBuilder star = new StringBuilder();
-        StringBuilder blank = new StringBuilder();
-        StringBuilder result = new StringBuilder();
-        for(int i = 0; i < input; i++) {
-            star.append("*");
-            blank.append(" ");
-        }
+
         for(int i = 1; i < input; i++) {
-            result.append(blank.substring(i)+star.substring(0,i)+"\n");
+            for(int j = 1; j < i; j++) bw.write(" ");
+            for(int j = 0; j < (input - i)*2 +1; j++) bw.write("*");
+            bw.write("\n");
         }
         for(int i = 0; i < input; i++) {
-            result.append(blank.substring(0,i)+star.substring(i)+"\n");
+            for(int j = 1; j < input - i; j++) bw.write(" ");
+            for(int j = 0; j < i * 2 + 1; j++) bw.write("*");
+            bw.write("\n");
         }
 
-        System.out.println(result);
+        bw.flush();
     }
 }
