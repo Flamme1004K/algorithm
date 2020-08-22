6db0d3f8b96b69af7ff48052c46347c503d3f7be
flamme <flamme1004@gmail.com>
2020-08-23 01:09:27 +0900

    오늘자 알고리즘

--- src/flamme/algorithm/Programmers/소수찾기.java
+++ src/flamme/algorithm/Programmers/소수찾기.java
@@ -26,15 +26,16 @@ public class 소수찾기 {
 
     public static int solution(int n) {
         int answer = 0;
-
         for(int i=2; i<=n; i++) {
-            for(int j=2; j<=i; j++) {
-                if(j%2==0) {
-
+            boolean flag = true;
+            for(int j=2; j<i; j++) {
+                if(i%j==0) {
+                    flag = false;
+                    break;
                 }
             }
+            if(flag==true) answer++;
         }
-
         return answer;
     }
 
