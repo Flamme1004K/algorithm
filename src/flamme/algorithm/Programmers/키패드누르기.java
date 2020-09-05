af0809c796cec3ba2567a409cc2367c569509fbd
flamme <flamme1004@gmail.com>
2020-09-05 22:55:14 +0900

    오늘자알고리즘

--- src/flamme/algorithm/Programmers/키패드누르기.java
+++ src/flamme/algorithm/Programmers/키패드누르기.java
@@ -5,35 +5,54 @@ package flamme.algorithm.Programmers;
  왼쪽 열 3개지 1,4,7 왼손 엄지
  오른쪽 열의 3개 3,6,9 오른손 엄지
  가운데 열 2,5,8,0 두 엄지손가락의 현재 키패드의 위치에서 더 가까운엄지 사용
- 4-1 만약 두 엄지손가락의 거리가 같다면  올느손잡이는 오른손 엄지 왼손잡이는 왼손엄
+ 4-1 만약 두 엄지손가락의 거리가 같다면  오른손잡이는 오른손 엄지 왼손잡이는 왼손 엄지
 */
 public class 키패드누르기 {
     public static void main(String[] args) throws Exception {
 
 
 
-        int[] arrayString =  {1,3,4,5,8,2,1,4,5,9,5};
+        int[] arrayString =  {1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5};
 
         solution(arrayString, "right");
     }
 
-    public static int[] solution(int[] arr, String handle) {
-
-        int[] answer = new int[arr.length];
+    public static String[] solution(int[] arr, String handle) {
+        String[] answer = new String[arr.length];
         int right = 0;
         int left = 0;
 
-        int[][] defaultRight = {{1,4,7}, {2,5,8,0},{3,6,9}};
+        for (int i = 0; i<arr.length; i++) {
+            if (arr[i] == 1 || arr[i] == 4 || arr[i] == 7) {
+                answer[i] = "L";
+                left = arr[i];
+            } else if (arr[i] == 3 || arr[i] == 6 || arr[i] ==9) {
+                answer[i] = "R";
+                right = arr[i];
+            } else if (arr[i] == 2 || arr[i] == 5 || arr[i] == 8 || arr[i] == 0) {
 
-        //첫번째 숫자
-        for(int i = 0; i< defaultRight.length; i++) {
-            for(int j = 0; j<defaultRight[i].length; j ++) {
+                int rightSum = Math.abs(right - arr[i]);
+                int leftSum = Math.abs(left - arr[i]);
 
+                if(leftSum == rightSum ) {
+                    if (handle.equals("right")) {
+                        answer[i] = "R";
+                        right = arr[i];
+                    } else if (handle.equals("left")) {
+                        answer[i] = "L";
+                        left = arr[i];
+                    }
+                }else if( rightSum < leftSum) {
+                    answer[i] = "R";
+                    right = arr[i];
+                } else {
+                    answer[i] = "L";
+                    left = arr[i];
+                }
             }
-        }
-
-        for (int i = 1; i<arr.length; i++) {
-            int a = arr[i];
+            System.out.println(answer[i]);
+            //"LRLLLRLLRRL"
+            //LRLLRRLLLRR
 
         }
 
