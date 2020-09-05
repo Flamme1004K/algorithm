dd1403cdf31e9ed83d595cfef5219d6e218f4a83
flamme <flamme1004@gmail.com>
2020-09-05 23:49:10 +0900

    오늘자알고리즘

--- src/flamme/algorithm/Programmers/키패드누르기.java
+++ src/flamme/algorithm/Programmers/키패드누르기.java
@@ -1,61 +1,85 @@
 package flamme.algorithm.Programmers;
 
+import java.awt.*;
+
+
 /*처음 왼손 엄지손가락 * , 오른손 엄지손가락 #  키패드위치에서 시작하며 엄지손가락을 사용하는 규칙
  엄지손가락은 상하좌우 4가지 방향 이동, 키패드 이동 한 칸을 거리로 1에 해당
  왼쪽 열 3개지 1,4,7 왼손 엄지
  오른쪽 열의 3개 3,6,9 오른손 엄지
  가운데 열 2,5,8,0 두 엄지손가락의 현재 키패드의 위치에서 더 가까운엄지 사용
  4-1 만약 두 엄지손가락의 거리가 같다면  오른손잡이는 오른손 엄지 왼손잡이는 왼손 엄지
+
+ 3* 0 + 1 =1 0,1
+ ..
+ 3 * 1 + 1 =4 1.1
+
+ 3 * 2 + 1 =7 2.1
 */
 public class 키패드누르기 {
     public static void main(String[] args) throws Exception {
 
 
 
-        int[] arrayString =  {1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5};
+        int[] arrayString =  {1, 3, 4, 5, 8, 2, 1, 4, 5, 9,5};
 
-        solution(arrayString, "right");
+
+        System.out.println(solution(arrayString, "right"));
     }
 
-    public static String[] solution(int[] arr, String handle) {
-        String[] answer = new String[arr.length];
-        int right = 0;
-        int left = 0;
+    public static String solution(int[] arr, String handle) {
+        StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i<arr.length; i++) {
-            if (arr[i] == 1 || arr[i] == 4 || arr[i] == 7) {
-                answer[i] = "L";
-                left = arr[i];
-            } else if (arr[i] == 3 || arr[i] == 6 || arr[i] ==9) {
-                answer[i] = "R";
-                right = arr[i];
-            } else if (arr[i] == 2 || arr[i] == 5 || arr[i] == 8 || arr[i] == 0) {
+        Point[] nums = new Point[10];
+        nums[0] = new Point(3, 1);
+        nums[1] = new Point(0, 0);
+        nums[2] = new Point(0, 1);
+        nums[3] = new Point(0, 2);
+        nums[4] = new Point(1, 0);
+        nums[5] = new Point(1, 1);
+        nums[6] = new Point(1, 2);
+        nums[7] = new Point(2, 0);
+        nums[8] = new Point(2, 1);
+        nums[9] = new Point(2, 2);
+        Point L = new Point(3, 0);
+        Point R = new Point(3, 2);
 
-                int rightSum = Math.abs(right - arr[i]);
-                int leftSum = Math.abs(left - arr[i]);
-
-                if(leftSum == rightSum ) {
+        for (int num : arr) {
+            if (num == 1 || num == 4 || num == 7) {
+                L.x = nums[num].x;
+                L.y = nums[num].y;
+                sb.append("L");
+            } else if (num == 3 || num == 6 || num == 9) {
+                R.x = nums[num].x;
+                R.y = nums[num].y;
+                sb.append("R");
+            } else {
+                int disL = Math.abs(L.x - nums[num].x) + Math.abs(L.y - nums[num].y);
+                int disR = Math.abs(R.x - nums[num].x) + Math.abs(R.y - nums[num].y);
+                if (disL == disR) {
                     if (handle.equals("right")) {
-                        answer[i] = "R";
-                        right = arr[i];
-                    } else if (handle.equals("left")) {
-                        answer[i] = "L";
-                        left = arr[i];
+                        R.x = nums[num].x;
+                        R.y = nums[num].y;
+                        sb.append("R");
+                    } else {
+                        L.x = nums[num].x;
+                        L.y = nums[num].y;
+                        sb.append("L");
                     }
-                }else if( rightSum < leftSum) {
-                    answer[i] = "R";
-                    right = arr[i];
                 } else {
-                    answer[i] = "L";
-                    left = arr[i];
+                    if (disR < disL) {
+                        R.x = nums[num].x;
+                        R.y = nums[num].y;
+                        sb.append("R");
+                    } else {
+                        L.x = nums[num].x;
+                        L.y = nums[num].y;
+                        sb.append("L");
+                    }
                 }
             }
-            System.out.println(answer[i]);
-            //"LRLLLRLLRRL"
-            //LRLLRRLLLRR
-
         }
 
-        return answer;
+        return sb.toString();
     }
 }
