e4996adde96846ed31e6a273ee1d5fed0e276c30
flamme <flamme1004@gmail.com>
2020-09-27 23:30:51 +0900

    오늘자 알고리즘 수정

--- src/flamme/algorithm/Programmers/문자열내림차순으로배치하기.java
+++ src/flamme/algorithm/Programmers/문자열내림차순으로배치하기.java
@@ -22,7 +22,7 @@ public String reverseStr(String str){
 
  */
 public class 문자열내림차순으로배치하기 {
-    public static void main(String[] args) throws Exception {√
+    public static void main(String[] args) throws Exception {
         BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
         System.out.println(solution(sc.readLine()));
     }
